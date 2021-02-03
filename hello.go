b1b631b169e713b7a7ae9d736f3f3727c26fdc81
Chawintee <Chawinteetea@gmail.com>
2021-02-03 21:43:35 +0700

    go use package

--- hello.go
+++ hello.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main () {
+	"rsc.io/quote"
+)
+
+func main() {
 	fmt.Println("Hello, World!")
 	fmt.Print("New line")
 	fmt.Print("New line2")
 	fmt.Println("New line 3")
 	fmt.Println("New line 4")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
 }

7d0588bdf6969b4ce886da06f82b0807acec3c2f
Chawintee <Chawinteetea@gmail.com>
2021-02-03 21:31:30 +0700

    go fmt.Println

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main () {
+	fmt.Println("Hello, World!")
+	fmt.Print("New line")
+	fmt.Print("New line2")
+	fmt.Println("New line 3")
+	fmt.Println("New line 4")
+}

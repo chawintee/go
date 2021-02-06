020c334cfbb5f60eadd50039447a2e9c5541e4a0
Chawintee Muennarong <chawinteetea@gmail.com>
2021-02-06 18:26:47 +0700

    forloop example

---
+++ day2GolangLoop.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func init1() {
+	fmt.Println("int... Golang Day2 Loop...")
+}
+
+func forLoop() {
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	init1()
+	fmt.Println("Hello GOLANG Loop...")
+	forLoop()
+}

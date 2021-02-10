9024942c91740cbe23d257a5462d6168857ab4c8
Chawintee <Chawinteetea@gmail.com>
2021-02-10 22:15:42 +0700

    SwichCaseSameOtherLangugeWhenUse-fallthrough-

--- day3SwitchCase.go
+++ day3SwitchCase.go
@@ -7,7 +7,7 @@ import (
 // switchCase type 1 is no Break different other language
 func switchCasetype1() {
 	i := 5
-	fmt.Println("Write", i, "to")
+	fmt.Print("Write", i, "to")
 	switch i {
 	case 1:
 		fmt.Println("Hello 1")
@@ -26,8 +26,35 @@ func switchCasetype1() {
 	}
 }
 
+func switchCaseType2() {
+	j := 3
+	switch j {
+	case 5:
+		fmt.Println("five not three")
+		fallthrough
+	case 4:
+		fmt.Println("four not three")
+		fallthrough
+	case 3:
+		fmt.Print("three ")
+		fallthrough
+	case 2:
+		fmt.Print("two ")
+		fallthrough
+	case 1:
+		fmt.Print("one ")
+		fallthrough
+	case 0:
+		fmt.Print("zero ")
+		fallthrough
+	default:
+		fmt.Print("Go!!!")
+	}
+}
+
 func main() {
 	fmt.Println("Hello Day3 Golang SwitchCase")
 	switchCasetype1()
+	switchCaseType2()
 
 }
